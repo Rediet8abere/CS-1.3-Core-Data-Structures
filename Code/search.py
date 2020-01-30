94d0b01e485cf4c8bf182eb09576e2496680c3ad
Rediet8abere <redietaberegesesse@gmail.com>
2020-01-29 16:03:14 -0800

    binrec

--- Code/search.py
+++ Code/search.py
@@ -4,8 +4,8 @@ def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
     # implement linear_search_iterative and linear_search_recursive below, then
     # change this to call your implementation to verify it passes all tests
-    # return linear_search_iterative(array, item)
-    return linear_search_recursive(array, item)
+    return linear_search_iterative(array, item)
+    # return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -28,11 +28,18 @@ def linear_search_recursive(array, item, index=0):
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
-    # print(len(str(bin(100))[2:]))
 
-    # 6 34 9
+
+
+    # implement binary_search_iterative and binary_search_recursive below, then
+    # change this to call your implementation to verify it passes all tests
+    # return binary_search_iterative(array, item)
+    return binary_search_recursive(array, item, 0, len(array)-1)
+
+
+def binary_search_iterative(array, item):
+    # TODO: implement binary search iteratively here
     array = sorted(array)
-    print("array Sorted", array)
     low = 0
     high = len(array)-1
     mid = (low + high) //2
@@ -46,29 +53,33 @@ def binary_search(array, item):
             low = mid + 1
             mid = (low + high) //2
 
-
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
-
-
-def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
     # once implemented, change binary_search to call binary_search_iterative
     # to verify that your iterative implementation passes all tests
 
 
-def binary_search_recursive(array, item, left=None, right=None):
+def binary_search_recursive(array, item, left, right):
     # TODO: implement binary search recursively here
-    pass
+    array = sorted(array)
+    low = left
+    high = right
+    mid = (low + high) //2
+    if item == array[mid]:
+        return mid
+    elif array[mid] > item:
+        high = mid - 1
+        mid = (low + high) //2
+        return binary_search_recursive(array, item, left=low, right=high)
+    elif array[mid] < item:
+        low = mid + 1
+        mid = (low + high) //2
+        return binary_search_recursive(array, item, left=low, right=high)
+
     # once implemented, change binary_search to call binary_search_recursive
     # to verify that your recursive implementation passes all tests
 
 def main():
-    num = [89, 2]
-    result = binary_search(num, 89)
+    num = [89, 2, 4, 5, 34, 56, 78, 90, 3]
+    result = binary_search(num, 5)
     # result2 = binary_search(num, 2)
     print("result", result)
     # print("result2", result2)
