d6128f5843e1847d1bf06a2ba048b6a64411b27f
Rediet8abere <redietaberegesesse@gmail.com>
2020-02-08 12:43:44 -0800

    code refactored

--- Code/strings.py
+++ Code/strings.py
@@ -1,105 +1,59 @@
 #!python
+class StringMan:
+    def __init__(self):
+        self.indexes = []
 
-def contains(text, pattern):
-    """Return a boolean indicating whether pattern occurs in text."""
-    assert isinstance(text, str), 'text is not a string: {}'.format(text)
-    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # Implement contains here (iteratively and/or recursively)
-    if len(pattern) == 0:
-        return True
-    index = 0
-    i = 0
-    while i < len(text):
-        if index < len(pattern):
+
+    def contains(self, text, pattern):
+        if len(pattern) == 0:
+            return True
+        self.indexes = []
+        self.find_all_indexes(text, pattern)
+        return self.indexes != []
+
+    def find_index(self, text, pattern):
+        if len(pattern) == 0:
+            return 0
+        self.indexes = []
+        self.find_all_indexes(text, pattern)
+        if len(self.indexes) >= 1:
+            return self.indexes[0]
+        else:
+            print("self.indexes", self.indexes)
+            return None
+
+    def find_all_indexes(self, text, pattern):
+        if len(pattern) == 0:
+            for i in range(len(text)):
+                self.indexes.append(i)
+            return self.indexes
+        self.indexes = []
+        index = 0
+        i = 0
+        while i < len(text):
             if pattern[index] == text[i]:
                 index += 1
                 i += 1
                 if index == len(pattern):
-                    return True
+                    self.indexes.append(i-len(pattern))
+                    if len(pattern) != 1:
+                        i -= (index - 1)
+                    index = 0
             elif index != 0:
                 i -= (index - 1)
                 index = 0
             else:
                 i += 1
-        else:
-            index = 0
-            if pattern[index] == text[i]:
-                index += 1
-    return False
-
-
-def find_index(text, pattern):
-    """Return the starting index of the first occurrence of pattern in text,
-    or None if not found."""
-    assert isinstance(text, str), 'text is not a string: {}'.format(text)
-    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # Implement find_index here (iteratively and/or recursively)
-    if len(pattern) == 0:
-        return 0
-
-    # if contains(text, pattern):
-    index = 0
-    i = 0
-    while i < len(text):
-        if index < len(pattern):
-            if pattern[index] == text[i]:
-                index += 1
-                i += 1
-                if index == len(pattern):
-                    return i-len(pattern)
-            elif index != 0:
-                i -= (index - 1)
                 index = 0
-            else:
-                i += 1
-        else:
-            index = 0
-            if pattern[index] == text[i]:
-                index += 1
-    return None
-
-
-def find_all_indexes(text, pattern):
-    """Return a list of starting indexes of all occurrences of pattern in text,
-    or an empty list if not found."""
-    assert isinstance(text, str), 'text is not a string: {}'.format(text)
-    assert isinstance(pattern, str), 'pattern is not a string: {}'.format(text)
-    # Implement find_all_indexes here (iteratively and/or recursively)
-    print("len", len(text))
-    indexes = []
-    if len(pattern) == 0:
-        for i in range(len(text)):
-            indexes.append(i)
-        return indexes
-    index = 0
-    i = 0
-    while i < len(text):
-        if pattern[index] == text[i]:
-            index += 1
-            i += 1
-            if index == len(pattern):
-                indexes.append(i-len(pattern))
-                if len(pattern) != 1:
-                    i -= (index - 1)
-                index = 0
-        elif index != 0:
-            i -= (index - 1)
-            index = 0
-        else:
-            i += 1
-            index = 0
-    return indexes
-
+        return self.indexes
 
 def test_string_algorithms(text, pattern):
-    found = contains(text, pattern)
-    print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
-    # Uncomment these lines after you implement find_index
-    index = find_index(text, pattern)
-    print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
-    # Uncomment these lines after you implement find_all_indexes
-    indexes = find_all_indexes(text, pattern)
-    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
+    check = StringMan()
+
+    print('contains({!r}, {!r}) => {}'.format(text, pattern, check.contains(text, pattern)))
+    print('find_index({!r}, {!r}) => {}'.format(text, pattern, check.find_index(text, pattern)))
+    print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, check.find_all_indexes(text, pattern)))
+
 
 
 def main():
--- Code/strings_test.py
+++ Code/strings_test.py
@@ -1,131 +1,146 @@
 #!python
 
-from strings import contains, find_index, find_all_indexes
+import strings
 import unittest
 
 
 class StringsTest(unittest.TestCase):
 
+
+    def test_StringMan_instance(self):
+        self.check = strings.StringMan()
+        assert self.check
+
     def test_contains_with_matching_patterns(self):
+        check = strings.StringMan()
         # Positive test cases (examples) with matching patterns
-        assert contains('abc', '') is True  # all strings contain empty string
-        assert contains('abc', 'a') is True  # single letters are easy
-        assert contains('abc', 'b') is True
-        assert contains('abc', 'c') is True
-        assert contains('abc', 'ab') is True  # multiple letters are harder
-        assert contains('abc', 'bc') is True
-        assert contains('abc', 'abc') is True  # all strings contain themselves
-        assert contains('aaa', 'a') is True  # multiple occurrences
-        assert contains('aaa', 'aa') is True  # overlapping pattern
+        assert check.contains('abc', '') is True  # all strings contain empty string
+        assert check.contains('abc', 'a') is True  # single letters are easy
+        assert check.contains('abc', 'b') is True
+        assert check.contains('abc', 'c') is True
+        assert check.contains('abc', 'ab') is True  # multiple letters are harder
+        assert check.contains('abc', 'bc') is True
+        assert check.contains('abc', 'abc') is True  # all strings contain themselves
+        assert check.contains('aaa', 'a') is True  # multiple occurrences
+        assert check.contains('aaa', 'aa') is True  # overlapping pattern
         # TODO: Write more positive test cases with assert is True statements
-        assert contains('..', '.') is True
-        assert contains('../n', './n') is True
-        assert contains('Rediet', 'Redi') is True
-        # assert contains('ooooooooohhhhh', 'oooohhhhh') is True
+        assert check.contains('..', '.') is True
+        assert check.contains('../n', './n') is True
+        assert check.contains('Rediet', 'Redi') is True
+        assert check.contains('ooooooooohhhhh', 'oooohhhhh') is True
 
     def test_contains_with_non_matching_patterns(self):
+        check = strings.StringMan()
         # Negative test cases (counterexamples) with non-matching patterns
-        assert contains('abc', 'z') is False  # remember to test other letters
-        assert contains('abc', 'ac') is False  # important to test close cases
-        assert contains('abc', 'az') is False  # first letter, but not last
-        assert contains('abc', 'abz') is False  # first 2 letters, but not last
+        assert check.contains('abc', 'z') is False  # remember to test other letters
+        assert check.contains('abc', 'ac') is False  # important to test close cases
+        assert check.contains('abc', 'az') is False  # first letter, but not last
+        assert check.contains('abc', 'abz') is False  # first 2 letters, but not last
         # TODO: Write more negative test cases with assert is False statements
-        assert contains('btzze', 'tze') is False
-        assert contains('aaa', 'a:''') is False
-        assert contains('greek', 'eik') is False
-        assert contains('hahahaha', 'hha') is False
-
+        assert check.contains('btzze', 'tze') is False
+        assert check.contains('aaa', 'a:''') is False
+        assert check.contains('greek', 'eik') is False
+        assert check.contains('hahahaha', 'hha') is False
+    #
     def test_contains_with_complex_patterns(self):
+        check = strings.StringMan()
         # Difficult test cases (examples) with complex patterns
-        assert contains('ababc', 'ab') is True  # multiple occurrences
-        assert contains('banana', 'na') is True  # multiple occurrences
-        assert contains('ababc', 'abc') is True  # overlapping prefix
-        assert contains('bananas', 'nas') is True  # overlapping prefix
+        assert check.contains('ababc', 'ab') is True  # multiple occurrences
+        assert check.contains('banana', 'na') is True  # multiple occurrences
+        assert check.contains('ababc', 'abc') is True  # overlapping prefix
+        assert check.contains('bananas', 'nas') is True  # overlapping prefix
         # TODO: Write more test cases that check complex patterns or edge cases
         # You'll need a lot more than this to test your algorithm's robustness
-        assert contains('hahahahahahaha', 'ha') is True
-        assert contains('ooooooooohhhhh', 'oooohhh') is True
-
+        assert check.contains('hahahahahahaha', 'ha') is True
+        assert check.contains('ooooooooohhhhh', 'oooohhh') is True
+    #
     def test_find_index_with_matching_patterns(self):
+        check = strings.StringMan()
         # Positive test cases (examples) with matching patterns
-        assert find_index('abc', '') == 0  # all strings contain empty string
-        assert find_index('abc', 'a') == 0  # single letters are easy
-        assert find_index('abc', 'b') == 1
-        assert find_index('abc', 'c') == 2
-        assert find_index('abc', 'ab') == 0  # multiple letters are harder
-        assert find_index('abc', 'bc') == 1
-        assert find_index('abc', 'abc') == 0  # all strings contain themselves
-        assert find_index('aaa', 'a') == 0  # multiple occurrences
-        assert find_index('aaa', 'aa') == 0  # overlapping pattern
+        assert check.find_index('abc', '') == 0  # all strings contain empty string
+        assert check.find_index('abc', 'a') == 0  # single letters are easy
+        assert check.find_index('abc', 'b') == 1
+        assert check.find_index('abc', 'c') == 2
+        assert check.find_index('abc', 'ab') == 0  # multiple letters are harder
+        assert check.find_index('abc', 'bc') == 1
+        assert check.find_index('abc', 'abc') == 0  # all strings contain themselves
+        assert check.find_index('aaa', 'a') == 0  # multiple occurrences
+        assert check.find_index('aaa', 'aa') == 0  # overlapping pattern
         # TODO: Write more positive test cases with assert equal int statements
-        assert find_index('hahahahahahaha', 'ha') == 0
-        assert find_index('bbc', 'bbc') == 0
-
+        assert check.find_index('hahahahahahaha', 'ha') == 0
+        assert check.find_index('bbc', 'bbc') == 0
+    #
     def test_find_index_with_non_matching_patterns(self):
+        check = strings.StringMan()
         # Negative test cases (counterexamples) with non-matching patterns
-        assert find_index('abc', 'z') is None  # remember to test other letters
-        assert find_index('abc', 'ac') is None  # important to test close cases
-        assert find_index('abc', 'az') is None  # first letter, but not last
-        assert find_index('abc', 'abz') is None  # first 2 letters, but not last
+        assert check.find_index('abc', 'z') is None  # remember to test other letters
+        assert check.find_index('abc', 'ac') is None  # important to test close cases
+        assert check.find_index('abc', 'az') is None  # first letter, but not last
+        assert check.find_index('abc', 'abz') is None  # first 2 letters, but not last
         # TODO: Write more negative test cases with assert is None statements
-        assert find_index('bbc', 'bcc') is None
-
+        assert check.find_index('bbc', 'bcc') is None
+    #
     def test_find_index_with_complex_patterns(self):
+        check = strings.StringMan()
         # Difficult test cases (examples) with complex patterns
-        assert find_index('ababc', 'abc') == 2  # overlapping prefix
-        assert find_index('bananas', 'nas') == 4  # overlapping prefix
-        assert find_index('abcabcabc', 'abc') == 0  # multiple occurrences
-        assert find_index('abcabcab', 'abc') == 0  # multiple occurrences
-        assert find_index('abcabcdef', 'abcd') == 3  # overlapping prefix
-        assert find_index('abcabcdef', 'abcdef') == 3  # overlapping prefix
-        assert find_index('abcabcdabcde', 'abcde') == 7  # overlapping prefix
-        assert find_index('abcabcdabcde', 'abcd') == 3  # multiple occurrences, overlapping prefix
-        assert find_index('abra cadabra', 'abra') == 0  # multiple occurrences
-        assert find_index('abra cadabra', 'adab') == 6  # overlapping prefix
+        assert check.find_index('ababc', 'abc') == 2  # overlapping prefix
+        assert check.find_index('bananas', 'nas') == 4  # overlapping prefix
+        assert check.find_index('abcabcabc', 'abc') == 0  # multiple occurrences
+        assert check.find_index('abcabcab', 'abc') == 0  # multiple occurrences
+        assert check.find_index('abcabcdef', 'abcd') == 3  # overlapping prefix
+        assert check.find_index('abcabcdef', 'abcdef') == 3  # overlapping prefix
+        assert check.find_index('abcabcdabcde', 'abcde') == 7  # overlapping prefix
+        assert check.find_index('abcabcdabcde', 'abcd') == 3  # multiple occurrences, overlapping prefix
+        assert check.find_index('abra cadabra', 'abra') == 0  # multiple occurrences
+        assert check.find_index('abra cadabra', 'adab') == 6  # overlapping prefix
         # TODO: Write more test cases that check complex patterns or edge cases
         # You'll need a lot more than this to test your algorithm's robustness
-        assert find_index('aaaahahahahaha', 'ha') == 4
-
+        assert check.find_index('aaaahahahahaha', 'ha') == 4
+    #
     def test_find_all_indexes_with_matching_patterns(self):
+        check = strings.StringMan()
         # Positive test cases (examples) with matching patterns
-        assert find_all_indexes('abc', '') == [0, 1, 2]  # all strings contain empty string
-        assert find_all_indexes('abc', 'a') == [0]  # single letters are easy
-        assert find_all_indexes('abc', 'b') == [1]
-        assert find_all_indexes('abc', 'c') == [2]
-        assert find_all_indexes('abc', 'ab') == [0]  # multiple letters are harder
-        assert find_all_indexes('abc', 'bc') == [1]
-        assert find_all_indexes('abc', 'abc') == [0]  # all strings contain themselves
-        assert find_all_indexes('aaa', 'a') == [0, 1, 2]  # multiple occurrences
-        assert find_all_indexes('aaa', 'aa') == [0, 1]  # overlapping pattern
+        assert check.find_all_indexes('abc', '') == [0, 1, 2]  # all strings contain empty string
+        assert check.find_all_indexes('abc', 'a') == [0]  # single letters are easy
+        assert check.find_all_indexes('abc', 'b') == [1]
+        assert check.find_all_indexes('abc', 'c') == [2]
+        assert check.find_all_indexes('abc', 'ab') == [0]  # multiple letters are harder
+        assert check.find_all_indexes('abc', 'bc') == [1]
+        assert check.find_all_indexes('abc', 'abc') == [0]  # all strings contain themselves
+        assert check.find_all_indexes('aaa', 'a') == [0, 1, 2]  # multiple occurrences
+        assert check.find_all_indexes('aaa', 'aa') == [0, 1]  # overlapping pattern
     #     # TODO: Write more positive test cases with assert equal list statements
-        assert find_all_indexes('hahahahahahaha', 'ha') == [0, 2, 4, 6, 8, 10, 12]
-        assert find_all_indexes('hahahahahahaha', 'haha') == [0, 2, 4, 6, 8, 10]
-
-
+        assert check.find_all_indexes('hahahahahahaha', 'ha') == [0, 2, 4, 6, 8, 10, 12]
+        assert check.find_all_indexes('hahahahahahaha', 'haha') == [0, 2, 4, 6, 8, 10]
+    #
+    #
     def test_find_all_indexes_with_non_matching_patterns(self):
+        check = strings.StringMan()
     #     # Negative test cases (counterexamples) with non-matching patterns
-        assert find_all_indexes('abc', 'z') == []  # remember to test other letters
-        assert find_all_indexes('abc', 'ac') == []  # important to test close cases
-        assert find_all_indexes('abc', 'az') == []  # first letter, but not last
-        assert find_all_indexes('abc', 'abz') == []  # first 2 letters, but not last
+        assert check.find_all_indexes('abc', 'z') == []  # remember to test other letters
+        assert check.find_all_indexes('abc', 'ac') == []  # important to test close cases
+        assert check.find_all_indexes('abc', 'az') == []  # first letter, but not last
+        assert check.find_all_indexes('abc', 'abz') == []  # first 2 letters, but not last
         # TODO: Write more negative test cases with assert equal list statements
-        # ...
+        assert check.find_all_indexes('abra ca da bra', 'adab') == []
 
+    #
     def test_find_all_indexes_with_complex_patterns(self):
+        check = strings.StringMan()
         # Difficult test cases (examples) with complex patterns
-        assert find_all_indexes('ababc', 'abc') == [2]  # overlapping prefix
-        assert find_all_indexes('bananas', 'nas') == [4]  # overlapping prefix
-        assert find_all_indexes('abcabcabc', 'abc') == [0, 3, 6]  # multiple occurrences
-        assert find_all_indexes('abcabcab', 'abc') == [0, 3]  # multiple occurrences
-        assert find_all_indexes('abcabcdef', 'abcd') == [3]  # overlapping prefix
-        assert find_all_indexes('abcabcdef', 'abcdef') == [3]  # overlapping prefix
-        assert find_all_indexes('abcabcdabcde', 'abcde') == [7]  # overlapping prefix
-        assert find_all_indexes('abcabcdabcde', 'abcd') == [3, 7]  # multiple occurrences, overlapping prefix
-        assert find_all_indexes('abra cadabra', 'abra') == [0, 8]  # multiple occurrences
-        # assert find_all_indexes('abra cadabra', 'adab') == [6]  # overlapping prefix
+        assert check.find_all_indexes('ababc', 'abc') == [2]  # overlapping prefix
+        assert check.find_all_indexes('bananas', 'nas') == [4]  # overlapping prefix
+        assert check.find_all_indexes('abcabcabc', 'abc') == [0, 3, 6]  # multiple occurrences
+        assert check.find_all_indexes('abcabcab', 'abc') == [0, 3]  # multiple occurrences
+        assert check.find_all_indexes('abcabcdef', 'abcd') == [3]  # overlapping prefix
+        assert check.find_all_indexes('abcabcdef', 'abcdef') == [3]  # overlapping prefix
+        assert check.find_all_indexes('abcabcdabcde', 'abcde') == [7]  # overlapping prefix
+        assert check.find_all_indexes('abcabcdabcde', 'abcd') == [3, 7]  # multiple occurrences, overlapping prefix
+        assert check.find_all_indexes('abra cadabra', 'abra') == [0, 8]  # multiple occurrences
+        assert check.find_all_indexes('abra cadabra', 'adab') == [6]  # overlapping prefix
         # TODO: Write more test cases that check complex patterns or edge cases
         # You'll need a lot more than this to test your algorithm's robustness
-        # ...
+        assert check.find_all_indexes('ab cab cabc', 'abc') == [8]
 
 
 if __name__ == '__main__':
